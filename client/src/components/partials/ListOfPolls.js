72fde57590487ac48f7fb464cb97d2bbd8b9f889
frann-y <fgranger00@gmail.com>
2019-03-05 17:43:36 +0000

    mapped fake data to user for the poll

--- client/src/components/partials/ListOfPolls.js
+++ client/src/components/partials/ListOfPolls.js
@@ -1,118 +1,54 @@
-import React from 'react';
+import React, {Component}from 'react';
 import { Row, Col, Container } from 'reactstrap';
 import { Card, Form} from 'react-bootstrap';
 
 //Profile Match container is made with reactstrap
 //The cards generated are hardCoded, these are made with reactBootstrap.
 //When we render we will delete tese hard coded nd import the fie from Profile Match card
-
-const ListOfPolls = () => (
-  <div>
-    <Container>
-      <Row>
-        <Col style={{ padding: '10px' }}></Col>
-        <Col xl={12}>
+//map over my card to render it
 
 
-{/*Poll Cards generated*/}
-            <Row style={{ padding: '10px' }}>
-              <Col style={{ padding: '10px' }}>
+const myCard = [{
+  userName: 'George',
+  title: 'Hey Bloogers',
+  description: "LUNCH! Let's Do It! 12:00pm! 4th floor!"
+}];
 
-              <Card border="success"style={{ width: '18rem' }}>
-                <Card.Header>Users Says:</Card.Header>
+class ListOfPolls extends Component {
+  render(){
+    return(
+      <div>
+        {myCard.map((data) =>
+          <div>
+            <Card border="success"style={{ width: '18rem' }}>
+              <Card.Header>{data.userName} Says:</Card.Header>
                 <Card.Body>
-                  <Card.Title>Hey Bloogers!</Card.Title>
+                  <Card.Title>{data.title}</Card.Title>
                   <Card.Text>
-                    LUNCH!
-                    Let's Do It! <br/>
-                    12:00pm! <br/>
-                    4th floor! <br/>
+                    {data.description}
                   </Card.Text>
                 </Card.Body>
-                <Card.Footer>
-                <Form>
-                  {['checkbox'].map(type => (
-                    <div key={`default-${type}`} className="mb-3">
-                      <Form.Check
-                        type={type}
-                        id={`default-${type}`}
-                        label={`Yes!`}
-                      />
+              <Card.Footer>
+              <Form>
+                {['checkbox'].map(type => (
+                <div key={`default-${type}`} className="mb-3">
+                  <Form.Check
+                  type={type}
+                  id={`default-${type}`}
+                  label={`Join!`}
+                  />
+                </div>
+                ))}
+              </Form>
+              </Card.Footer>
+            </Card>
+          </div>
+        )}
+      </div>
+    )
+  }
+}
 
-                    </div>
-                  ))}
-                  </Form>
-                </Card.Footer>
-              </Card>
-
-              </Col>
-              <Col style={{ padding: '10px' }}>
-
-              <Card border="success"style={{ width: '18rem' }}>
-                <Card.Header>Users Says:</Card.Header>
-                <Card.Body>
-                  <Card.Title>Hey Bloogers!</Card.Title>
-                  <Card.Text>
-                    LUNCH!
-                    Let's Do It! <br/>
-                    12:00pm! <br/>
-                    4th floor! <br/>
-                  </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                <Form>
-                  {['checkbox'].map(type => (
-                    <div key={`default-${type}`} className="mb-3">
-                      <Form.Check
-                        type={type}
-                        id={`default-${type}`}
-                        label={`Yes!`}
-                      />
-
-                    </div>
-                  ))}
-                  </Form>
-                </Card.Footer>
-              </Card>
-              <br />
-
-              </Col>
-              <Col style={{ padding: '10px' }}>
-
-              <Card border="success"style={{ width: '18rem' }}>
-                <Card.Header>Users Says:</Card.Header>
-                <Card.Body>
-                  <Card.Title>Hey Bloogers!</Card.Title>
-                  <Card.Text>
-                    LUNCH!
-                    Let's Do It! <br/>
-                    12:00pm! <br/>
-                    4th floor! <br/>
-                  </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                <Form>
-                  {['checkbox'].map(type => (
-                    <div key={`default-${type}`} className="mb-3">
-                      <Form.Check
-                        type={type}
-                        id={`default-${type}`}
-                        label={`Yes!`}
-                      />
-                    </div>
-                  ))}
-                  </Form>
-                </Card.Footer>
-              </Card>
-
-              </Col>
-            </Row>
-        </Col>
-        <Col></Col>
-      </Row>
-    </Container>
-  </div>
-);
 
 //Exporting the File
 export default ListOfPolls;
