767a49bb2ac562a11abc543ec070462c96b50ef9
vagrant <nathalang_t@hotmail.ca>
2019-03-06 23:51:20 +0000

    sidebar population

--- client/src/components/partials/Sidebar.js
+++ client/src/components/partials/Sidebar.js
@@ -1,75 +1,75 @@
 import React, { Component } from "react";
 import { slide as Menu } from 'react-burger-menu'
 import Image from 'react-bootstrap/Image'
-import { Query } from 'react-apollo';
-import gql from 'graphql-tag';
 import '../../assets/styles/SideBar.css'
+import axios from 'axios'
 
-
-const USERS_QUERY = gql`
-  query {
-    user(firstName: "Mister") {
-        firstName
-        lastName
-        email
-        interests {
-            topic
-        }
-    }
-}`
 
 class SideBar extends Component {
-  showSettings(event) {
-    event.preventDefault();
+  state = {
+    users: [],
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:3001/api/v1/users")
+      .then(response => {
+        console.log(response);
+        this.setState({
+          users: response.data
+        });
+      })
+      .catch(error => console.log(error));
   }
 
   render() {
     return (
-      <Query query={USERS_QUERY}>
-        {({ loading, error, data }) => {
-          if (loading) return <div>Fetching..</div>
-          if (error) return <div>Error!</div>
-          return (
-            <div>
-              return <div key={data.user.id}>
-                <div pageWrapId={"page-wrap"}>
-                  <main id="page-wrap">
-                    <Menu
-                      isOpen={true}
-                      noOverlay
-                      customBurgerIcon={false}
-                      customCrossIcon={false}
-                    >
-                      <div>
-                        <Image
-                          src={`https://robohash.org/${data.user.email}.png?set=set4`}
-                          roundedCircle
-                          fluid
-                        />
-                      </div>
-                      <div style={{ padding: '20px' }}>
-                        <h3 id="about" className="menu-item">{data.user.firstName} {data.user.lastName}</h3>
-                        <br />
-                        <h4 id="about" className="menu-item"> {data.user.email} </h4>
-                        <br />
-                        <h4 id="about" className="menu-item"> Interests </h4>
-                        <br />
-                        <ul>{data.user.interests.map((el =>
-                          <li id="contact" className="menu-item">{el.topic}</li>
-                        ))}
-                        </ul>
-                      </div>
-                    </Menu>
-                  </main>
-                </div>
-              </div>
-
-            </div>
-          )
-        }}
-      </Query>
-    )
+      <div className="App">
+        <h1>Users</h1>
+        {this.state.users.map(user =>
+          <div key={user.id} value={user.email}>{user.email}</div>
+        )}
+      </div>
+    );
   }
 }
+
+
+//   render() {
+//     return (
+//       <div>
+//         return <div key={users.id}>
+//           <div pageWrapId={"page-wrap"}>
+//             <main id="page-wrap">
+//               <Menu
+//                 isOpen={true}
+//                 noOverlay
+//                 customBurgerIcon={false}
+//                 customCrossIcon={false}
+//               >
+//                 <div>
+//                   <Image
+//                     src={`https://robohash.org/${users.email}.png?set=set4`}
+//                     roundedCircle
+//                     fluid
+//                   />
+//                 </div>
+//                 <div style={{ padding: '20px' }}>
+//                   <h3 id="about" className="menu-item">{users.first_name} {users.last_name}</h3>
+//                   <br />
+//                   <h4 id="about" className="menu-item"> {users.email} </h4>
+//                   <br />
+//                   <h4 id="about" className="menu-item"> Interests </h4>
+//                   <br />
+//                 </div>
+//               </Menu>
+//             </main>
+//           </div>
+//         </div>
+
+//       </div>
+//     )
+//   }
+// }
 
 export default SideBar;
