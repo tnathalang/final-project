7162d7b778cefb264a8bfabe543cec7521c83fb7
Tom Na <nathalang_t@hotmail.ca>
2019-03-08 11:27:27 -0500

    new auth method with bcrypt

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -10,5 +10,10 @@ Rails.application.routes.draw do
     end
 end
 
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  get '/profile' to: "users#profile"
+
+
 
 end
