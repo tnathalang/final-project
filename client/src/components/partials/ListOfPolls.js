f39b5a1849500cd4fa00c5f507580f69723574de
frann-y <fgranger00@gmail.com>
2019-03-11 22:02:07 +0000

    changed for carousel

--- client/src/components/partials/ListOfPolls.js
+++ client/src/components/partials/ListOfPolls.js
@@ -26,8 +26,8 @@ class ListOfPolls extends React.Component {
         {
           breakpoint: 1024,
           settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
+            slidesToShow: 1,
+            slidesToScroll: 1,
             infinite: true,
             dots: true
           }
@@ -35,8 +35,8 @@ class ListOfPolls extends React.Component {
         {
           breakpoint: 600,
           settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
+            slidesToShow: 1,
+            slidesToScroll: 1,
             initialSlide: 2
           }
         },
