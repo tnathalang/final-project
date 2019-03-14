e0e35874519d5e5eefa117e97538d729a05e052f
vagrant <nathalang_t@hotmail.ca>
2019-03-14 15:19:42 +0000

    poll placeholder names

--- client/src/components/partials/NewPoll.js
+++ client/src/components/partials/NewPoll.js
@@ -15,68 +15,68 @@ class NewPoll extends React.Component {
   }
 
   //Handle Poll Change and Submit
-    handleOnChange = event => {
-      this.setState({
-        [event.target.name]: event.target.value //[target.name] is es6 , name is applied to the input. mostly square brakets
-      })
+  handleOnChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value //[target.name] is es6 , name is applied to the input. mostly square brakets
+    })
+  }
+
+  onSubmit = event => {
+    event.preventDefault();
+    let params = {
+      host_id: 1,
+      ...this.state
     }
 
-    onSubmit = event => {
-      event.preventDefault();
-      let params = {
-        host_id: 1,
-        ...this.state
-      }
+    this.props.submitNewPolls(params);
 
-      this.props.submitNewPolls(params);
+    this.setState({ title: '', description: '' }); //clear the field
+  };
 
-      this.setState({ title: '', description: '' }); //clear the field
-    };
-
-    // value={this.state.titlename}
-    // onChange= {this.handlePollChange}
-    // onKeyUp= {this.handlePollSubmit}??
+  // value={this.state.titlename}
+  // onChange= {this.handlePollChange}
+  // onKeyUp= {this.handlePollSubmit}??
 
 
-  render () {
+  render() {
 
     return (
-          // Form for testing
-          <div>
-            <Container
-              className= 'bg-light border-radius-3 ma-4 shadow'
-              style={{width: '75%', padding: '30px', borderRadius: '10px'}}>
+      // Form for testing
+      <div>
+        <Container
+          className='bg-light border-radius-3 ma-4 shadow'
+          style={{ width: '75%', padding: '30px', borderRadius: '10px' }}>
 
-                  <Form onSubmit={this.onSubmit}
-                  >
-                    <Form.Group controlId="exampleForm.ControlTextarea1">
+          <Form onSubmit={this.onSubmit}
+          >
+            <Form.Group controlId="exampleForm.ControlTextarea1">
 
-                      <Form.Label></Form.Label>
-                      <Form.Control
-                        name= "title"
-                        type="text"
-                        placeholder="Title"
-                        value={this.state.title}
-                        onChange={this.handleOnChange}
-                      />
-                      <br/ >
-                      <Form.Control
-                        as="textarea"
-                        size="lg"
-                        name = "description"
-                        type="text"
-                        placeholder="What would you like to do?"
-                        value = {this.state.description}
-                        onChange={this.handleOnChange}
-                        rows="3" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                      Submit
+              <Form.Label></Form.Label>
+              <Form.Control
+                name="title"
+                type="text"
+                placeholder="Give a date!"
+                value={this.state.title}
+                onChange={this.handleOnChange}
+              />
+              <br />
+              <Form.Control
+                as="textarea"
+                size="lg"
+                name="description"
+                type="text"
+                placeholder="What would you like to do?"
+                value={this.state.description}
+                onChange={this.handleOnChange}
+                rows="3" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
                     </Button>
-                  </Form>
+          </Form>
 
-            </Container>
-          </div>
+        </Container>
+      </div>
     )
   }
 }
